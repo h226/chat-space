fde8342e8d6e792fad61273b8ecb46c7d7436a55
h226 <nohmi.a.number14@gmail.com>
2019-10-30 13:09:21 +0900

    問題5-done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
-  before_action : set_group
+  before_action :set_group
 
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
