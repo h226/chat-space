f2cff2e3bfdea22e67ffaf23f1763c0fd3cd8723
h226 <nohmi.a.number14@gmail.com>
2019-11-07 18:37:41 +0900

    Update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -47,7 +47,6 @@ $(document).on('turbolinks:load', function(){
     })
   })
 
-$(document).on('turbolinks:load', function(){
   var reloadMessages = function() {
     var href = 'api/messages#index {:format=>"json"}'
     var last_message_id = $('.messages__message:last').data('message-id'); 
@@ -72,4 +71,3 @@ $(document).on('turbolinks:load', function(){
   if (window.location.href.match(/\/groups\/\d+\/messages/)){
   setInterval(reloadMessages, 5000);
 }});
-})
